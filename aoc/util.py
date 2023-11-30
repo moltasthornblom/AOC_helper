fefb94694719fdd4494289539a53c3136e475f97
Moltas Thörnblom <moltas.thornblom@hotmail.com>
2023-11-30 11:04:26 +0100

    fix: blck formatter

--- aoc/util.py
+++ aoc/util.py
@@ -1,5 +1,6 @@
 """Basic utility for the project"""
+
+
 def get_diff_date(date1, date2):
     """Get number of days between two dates"""
     return (date2-date1).days+1
- 
--- gen_boiler.py
+++ gen_boiler.py
@@ -1,7 +1,7 @@
 """Generate AOC boilerplate"""
 from aoc.aoc_controller import get_current_day_and_year
 
-day =  get_current_day_and_year()[0]
+day = get_current_day_and_year()[0]
 with open("template.txt", "r", encoding="utf-8") as f:
     file = f.read()
     new = file.replace("{day_here}", str(day))
